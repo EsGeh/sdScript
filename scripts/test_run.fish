edd473670a14e80f163a9277ee447719ad9f7d30
EsGeh <SamuelGfroerer@googlemail.com>
2019-03-14 22:09:58 +0100

    help system works, simplified all, updated scripts and README

--- scripts/test_run.fish
+++ scripts/test_run.fish
@@ -5,12 +5,7 @@ set SCRIPTS_DIR (dirname (readlink -m (status filename)))
 
 set doc_dir $BASE_DIR/doc
 
-# where to install:
-set install $doc_dir/install
-set install_legacy $doc_dir/install_legacy
-
-
 pd -noaudio \
 	-noprefs \
-	-path "$install" \
-	"$doc_dir/sdScript_doc.pd"
+	-path "$doc_dir" \
+	"$doc_dir/sdScript-help.pd"
